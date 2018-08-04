a41ccfa345bbc0a4f10630d6cf185b6d0249c449
oscarotero <oom@oscarotero.com>
2018-08-04 11:39:09 +0200

    Code style

--- src/Cors.php
+++ src/Cors.php
@@ -6,7 +6,6 @@ namespace Middlewares;
 use Middlewares\Utils\Traits\HasResponseFactory;
 use Neomerx\Cors\Contracts\AnalysisResultInterface;
 use Neomerx\Cors\Contracts\AnalyzerInterface;
-use Psr\Http\Message\ResponseFactoryInterface;
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
 use Psr\Http\Server\MiddlewareInterface;
